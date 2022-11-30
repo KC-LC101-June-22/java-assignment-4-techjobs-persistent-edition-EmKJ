b9ad660443a24fff6e82b1228a69524ced13881a
EmKJ <ekjacksondev@outlook.com>
2022-11-30 17:44:25 -0600

    changes to fix grading errors

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -47,7 +47,7 @@ public class EmployerController {
         }
     }
 
-    @GetMapping
+    @GetMapping("")
     public String index(Model model){
         model.addAttribute("employers", employerRepository.findAll());
         return "employers/index";
