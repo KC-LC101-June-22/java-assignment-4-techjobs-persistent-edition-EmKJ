a8c8806a356cc3dfe4bdb004af26b06bbc4bcea7
EmKJ <ekjacksondev@outlook.com>
2022-11-30 16:58:59 -0600

    wrote last sql query

--- queries.sql
+++ queries.sql
@@ -1,13 +1,14 @@
 -- Part 1: Test it with SQL
--- id:INT
--- employer:VARCHAR(255)
--- name:VARCHAR(255)
--- skills:VARCHAR(255)
+-- id:INT employer:VARCHAR(255) name:VARCHAR(255) skills:VARCHAR(255);
 
 -- Part 2: Test it with SQL
---SELECT name FROM employer WHERE location = "St. Louis City";
+-- SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
---DROP TABLE `tech_jobs`.`job`
+-- DROP TABLE `tech_jobs`.`job`;
 
 -- Part 4: Test it with SQL
+-- SELECT DISTINCT name
+-- FROM tech_jobs.skill
+-- RIGHT JOIN tech_jobs.job_skills ON tech_jobs.job_skills.skills_id = tech_jobs.skill.id
+-- ORDER BY name ASC;
